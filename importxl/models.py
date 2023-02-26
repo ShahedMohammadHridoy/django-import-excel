84323f38cb1a71922e1437fec1b72187592ae995
Shahed <shahed_hridoy@outlook.com>
2023-02-26 19:52:37 +0600

    model

--- importxl/models.py
+++ importxl/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    std_id = models.CharField(
+        max_length=11, null=True, blank=True
+    )
+    student_name = models.CharField(
+        max_length=100, null=True, blank=True
+    )
+    contact_number = models.CharField(
+        max_length=11, null=True, blank=True
+    )
+
+    def __str__(self):
+        return self.student_name
